4757831b8a4ea28fdbd88945a8563d54ea39daf4
SaarthakTuli <saarthaktuli@gmail.com>
2023-03-08 19:32:37 +0530

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,8 +23,9 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer">
-            Saarthak Tuli
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+            Saarthak Tuli &nbsp;
+            <span className="sm:block hidden"> | Student</span>
           </p>
         </Link>
 
