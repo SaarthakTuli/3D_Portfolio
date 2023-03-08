1b003f4e265d066a7982da7e1050dbb8b2c5924f
SaarthakTuli <saarthaktuli@gmail.com>
2023-03-08 20:26:51 +0530

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,28 +1,69 @@
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
+
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
+  const [globalMousePos, setGlobalMousePos] = useState({ x: 0, y: 2 });
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      setGlobalMousePos({
+        x: event.clientX,
+        y: event.clientY,
+      });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
   return (
-    <section className="relative w-full h-screen mx-auto">
+    <section className={`relative w-full h-screen mx-auto`}>
+      {/* Top Details */}
       <div
-        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flkex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
-        {/* Circle and Line  */}
+        {/* Circle and Line */}
         <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I am <span className="text-[#915eff]">Saarthak Tuli</span>
+            Hi, I'm <span className="text-[#915EFF]">Saarthak Tuli</span>
           </h1>
-
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop MERN Stack Applications & SwiftUI Apps.
+            I develop MERN Stack Applications &
+            <br className="sm:block hidden" />
+            SwiftUI + Firebase Apps
           </p>
         </div>
+      </div>
+
+      {/* Computer 3d Model */}
+      <ComputersCanvas />
+
+      {/* Next Section Button */}
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          {" "}
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.div
+              animate={{ y: [0, 24, 0] }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
       </div>
     </section>
   );
