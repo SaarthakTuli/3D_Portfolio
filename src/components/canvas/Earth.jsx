78e13621c8163f75165207ac4023fde366cbb78e
SaarthakTuli <saarthaktuli@gmail.com>
2023-03-10 01:49:35 +0530

    contact

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import Loader from "../Loader";
 
 const Earth = () => {
-  return (
-    <div>Earth</div>
-  )
-}
+  const earth = useGLTF("./planet/scene.gltf");
+  return <div>Earth</div>;
+};
 
-export default Earth
+const EarthCanvas = () => {
+  return (
+    <Canvas
+      shadows
+      frameloop="demand"
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{}}
+    >
+      <Suspense fallback={<Loader />}>
+        <OrbitControls
+          autoRotate={true}
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+
+        <Earth />
+      </Suspense>
+    </Canvas>
+  );
+};
+
+export default EarthCanvas;
