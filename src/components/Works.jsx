52e13766c05b0641c95152be9c9003a95d3caa21
SaarthakTuli <saarthaktuli@gmail.com>
2023-03-10 00:00:49 +0530

    works

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,6 +1,7 @@
 import Tilt from "react-tilt";
 import { motion } from "framer-motion";
 import { styles } from "../styles.js";
+import { github } from "../assets";
 
 const Works = () => {
   return <div>Works</div>;
