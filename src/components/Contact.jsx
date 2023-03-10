0b86d90f950ee3310a4609566fbb3124fb7253f9
SaarthakTuli <saarthaktuli@gmail.com>
2023-03-11 00:43:45 +0530

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,10 +7,6 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion.js";
 
-//FOj8OLbA1Ch1oFlDf
-// template_9fu3bgu
-// service_comxjjp
-
 const Contact = () => {
   const formRef = useRef();
 
