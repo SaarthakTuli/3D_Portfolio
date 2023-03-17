9251351e97537ec6ab82f09d9ad7428647a25da5
SaarthakTuli <saarthaktuli@gmail.com>
2023-03-17 23:22:48 +0530

    constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -42,11 +42,11 @@ export const navLinks = [
 
 const services = [
   {
-    title: "Web Developer",
+    title: "MERN Stack Developer",
     icon: web,
   },
   {
-    title: "React Native Developer",
+    title: "IOS Developer",
     icon: mobile,
   },
   {
@@ -54,7 +54,7 @@ const services = [
     icon: backend,
   },
   {
-    title: "Content Creator",
+    title: "UI/UX Designer",
     icon: creator,
   },
 ];
